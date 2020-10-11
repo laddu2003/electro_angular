8124303f677c58e46c059e8701930e86a4bbef94
Gaurav Palaspagar <gaurav.palaspagar@hotmail.com>
2020-10-11 15:25:44 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,15 @@ export class AppComponent {
     this.translate.setDefaultLang("en");
     this.token = _token.isLoggedIn();
     console.log(this.token);
-    // this.ipcRenderer.ipcRenderer.on("version", (res) => {
-    //   console.log(res);
-    // });
-    // this.electronService.ipcRenderer.on("version", (res) => {
-    //   console.log(res);
-    // });
     this.electronService.ipcRenderer.send("client-app-load", "client-app-load");
     this.electronService.ipcRenderer.on("version", (e, text) => {
       console.log("app version ", text);
     });
     this.electronService.ipcRenderer.on("message", (e, text) => {
       console.log("message ", text);
+    });
+    this.electronService.ipcRenderer.on("download-progress", (e, text) => {
+      console.log("download-progress ", text);
     });
   }
 }
